20e30f5fe7de8be7b5e777b71074d79c4b1aa1da
znareak <libardojesusrengifo129@gmail.com>
2021-02-03 21:15:05 +0100

    feat:  Code documentation

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,5 +1,11 @@
 import { COLUMN_CLASSNAME } from "./config.js";
 
+/**
+ * Set atribute to the element
+ * @param {HTMLElement} e - Node of DOM
+ * @param {object} props - An object with the properties
+ * @return {HTMLElement} - The node with the properties
+ */
 export function assigns(e, props) {
   for (const [k, v] of Object.entries(props)) {
     e.setAttribute(k, v);
@@ -7,15 +13,32 @@ export function assigns(e, props) {
   return e;
 }
 
+/**
+ * Create a new node DOM
+ * @param {string} tag - Tag of HTML node
+ * @param {object} props -  An object with the properties
+ * @return {HTMLElement} - The node with the properties
+ */
 export function element(tag, props) {
   const e = document.createElement(tag);
   return assigns(e, props);
 }
 
+/**
+ * Check if it has an attribute
+ * @param {object} obj - The object
+ * @param {string} prop -  the property to look for
+ * @return {boolean} - if prop is in obj
+ */
 export function hasProp(obj, prop) {
   return obj.hasOwnProperty(prop);
 }
 
+/**
+ * Create DOM nodes to make the columns
+ * @param {number} count - The count columns
+ * @return {object} - An object that has DOM nodes accessible by indexes
+ */
 export function makeColumns(count) {
   const columns = {};
   for (let i = 0; i < count; i++) {
@@ -24,15 +47,21 @@ export function makeColumns(count) {
   return columns;
 }
 
+/**
+ * Returns the origin from which the image will be searched
+ * @param {object} item - The object to set the source source of the image
+ * @return {string|object} - An object or chain depending on the slow loading flag
+ */
 export function getSource(item) {
   if (hasProp(item, "src")) return item.src;
   return item;
 }
 
-export function hasPropObj(obj) {
-  return (prop) => hasProp(obj, prop);
-}
-
+/**
+ * Add a node in the father
+ * @param {HTMLElement} father - The parent node
+ * @param {HTMLElement} child - The child node
+ */
 export function append(father, child) {
   father.appendChild(child);
 }
